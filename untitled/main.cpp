96d1f56882e3115a7f46f3385260a9a834358d1a
scarabush_scarabush <snusikfrescus@gmail.com>
2025-01-09 12:57:16 +0300

    some

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -3,11 +3,19 @@
 
 using namespace std;
 
+int N = 5;
+
 int main()
 {
     int a = 5;
     auto some = make_unique<int>(5);
     cout << "Hello World!" << endl;
+
+    int arr[5] = {1, 3, 5, 4, 5};
+
+    for (int i = 1; i < 6; i++) {
+        cout << arr[i] << endl;
+    }
 
     return EXIT_SUCCESS;
 }
